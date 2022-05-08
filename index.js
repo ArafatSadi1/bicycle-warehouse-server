1cc565a17efc0ff44c4144bf1b8b109b71c5061c
ArafatSadi1 <arafatsadi1@gmail.com>
2022-05-08 23:31:05 +0600

    without jwt

--- index.js
+++ index.js
@@ -4,28 +4,12 @@ const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-// const res = require("express/lib/response");
 require("dotenv").config();
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-function verifyJWT(req, res, next) {
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    return res.status(401).send({ message: "Unauthorized access" });
-  }
-  const token = authHeader.split(" ")[1];
-  console.log(token);
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(403).send({ message: "Forbidden access" });
-    }
-    req.decoded = decoded;
-    next();
-  });
-}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.e9exc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -40,14 +24,6 @@ async function run() {
     const bicycleCollection = client.db("bicycle").collection("products");
     const myProductsCollection = client.db("bicycle").collection("myProducts");
 
-    // JWT auth
-    app.post("/login", async (req, res) => {
-      const user = req.body;
-      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: "1d",
-      });
-      res.send({ accessToken });
-    });
 
     // products api
     app.get("/products", async (req, res) => {
@@ -97,17 +73,12 @@ async function run() {
     });
 
     // find myProducts
-    app.get("/myProducts", verifyJWT, async (req, res) => {
-      const decodedEmail = req.decoded.email;
+    app.get("/myProducts", async (req, res) => {
       const email = req.query.email;
-      if (decodedEmail === email) {
         const query = { email: email };
         const filter = myProductsCollection.find(query);
         const myProducts = await filter.toArray();
         res.send(myProducts);
-      } else {
-        return res.status(403).send({ message: "Forbidden Access" });
-      }
     });
 
     // myProduct api
